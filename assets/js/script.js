8e8961371dfcee698e7189025428a859380821bb
volosevych <volosevichh@gmail.com>
2025-02-09 14:18:02 -0700

    Adjustment for cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,19 @@ hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
     hamburger.classList.toggle('open');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".fade-in-section");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.2 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
